e3b0c3dfde6bdf633e896afdde375e35dba3c110
Rizki Irawan <rizkiirawan22998@gmail.com>
2022-07-02 20:53:31 +0700

    update admin article view

---
+++ resources/views/pages/admin/article/create.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.main', ['title' => 'Tambah Artikel', 'article' => 'active', 'data' => 'active', 'menu' =>
+'menu-open'])
+@section('content-header')
+<div class="container-fluid">
+    <div class="row mb-2">
+        <div class="col-sm-12">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item active">Tambah Artikel</li>
+            </ol>
+        </div>
+    </div>
+</div><!-- /.container-fluid -->
+@endsection
+@section('content')
+<div class="container-fluid">
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <div class="card">
+                <div class="card-header bg-primary">
+                    <h3>Tambah Artikel</h3>
+                </div>
+                <div class="card-body p-5">
+                    <form action="{{ route('admin.artikel.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="form-group">
+                            <label for="title" class="col-sm-6">Judul</label>
+                            <div class="col-sm-12">
+                                <input type="text" class="form-control @error('title') is-invalid @enderror" id="title"
+                                    name="title" value="{{ old('title') }}">
+                                @error('title')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="content" class="col-sm-6">Isi</label>
+                            <div class="col-sm-12 @error('content') is-invalid @enderror">
+                                <div id="editor">
+                                </div>
+                                <input type="hidden" id="content" name="content">
+                            </div>
+                            @error('content')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <label for="image" class="col-sm-6">Gambar</label>
+                            <div class="col-sm-12">
+                                <img src="" class="img-fluid col-sm-4 mb-2" alt="Gambar Artikel" id="img_preview">
+                                <input type="file" class="form-control-file @error('image') is-invalid @enderror"
+                                    id="image" name="image" onchange="previewImage()">
+                                @error('image')
+                                <div class="invalid-feedback">
+                                    {{ $message }}
+                                </div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <button class="btn btn-primary" type="submit"><i class="fas fa-save"></i>
+                                Simpan</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+@push('js')
+<script>
+    var toolbarOptions = [['bold', 'italic', 'underline'],[{ 'header': [] }],[{'align' : []}]];
+    var quill = new Quill('#editor', {
+    modules: {
+        toolbar: toolbarOptions
+    },
+    theme: 'snow'
+    });
+
+    quill.on('text-change', function(delta, oldDelta, source) {
+        document.getElementById("content").value = quill.root.innerHTML;
+    });
+
+    $('#img_preview').hide()
+
+    function previewImage() {
+        $('#img_preview').show()
+        const article_image = document.querySelector('#image');
+        const imgPreview = document.querySelector('#img_preview');
+        imgPreview.style.display = 'block';
+        const oFReader = new FileReader();
+        oFReader.readAsDataURL(article_image.files[0]);
+        oFReader.onload = function(oFEvent) {
+            imgPreview.src = oFEvent.target.result;
+        }
+    }
+</script>
+@endpush
---
+++ resources/views/pages/admin/article/index.blade.php
@@ -0,0 +1,162 @@
+@extends('layouts.main', ['title' => 'Data Artikel', 'article' => 'active', 'data' => 'active', 'menu' => 'menu-open'])
+@section('content-header')i
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Data Artikel</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
+                    <li class="breadcrumb-item active">Data Artikel</li>
+                </ol>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
+@section('content')
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <a href="{{ route('admin.artikel.create') }}" class="btn btn-primary mb-2"><i
+                            class="fas fa-plus-circle"></i>
+                        Tambah Data</a>
+                </div>
+                <div class="card">
+                    <div class="card-body">
+                        <table style="width: 100%" id="articleTable"
+                            class="table table-bordered table-hover table-striped responsive">
+                            <thead class="bg-primary text-center">
+                                <tr>
+                                    <th width="5%">No</th>
+                                    <th>Gambar</th>
+                                    <th>Judul</th>
+                                    <th>Tanggal</th>
+                                    <th>Isi</th>
+                                    <th>Author</th>
+                                    <th width="10%"><i class="fas fa-cog"></i></th>
+                                </tr>
+                            </thead>
+                            <tfoot class="bg-primary text-center">
+                                <tr>
+                                    <th width="5%">No</th>
+                                    <th>Gambar</th>
+                                    <th>Judul</th>
+                                    <th>Tanggal</th>
+                                    <th>Isi</th>
+                                    <th>Author</th>
+                                    <th width="10%"><i class="fas fa-cog"></i></th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+@includeIf('pages.student.modal')
+@endsection
+@push('js')
+<script type="text/javascript">
+    var Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+        });
+
+        $.ajaxSetup({
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        @if (session('success'))
+            Toast.fire({
+            icon: 'success',
+            title: '{!! session('success') !!}'
+            });
+        @endif
+
+        table = $('#articleTable').DataTable({
+            processing: true,
+            autoWidth: false,
+            serverSide: true,
+            ajax: {
+                url: '{{ route('admin.artikel.index') }}',
+            },
+            columns: [{
+                    data: 'DT_RowIndex',
+                    searchable: false,
+                    sortable: false
+                },
+                {
+                    data: 'image'
+                },
+                {
+                    data: 'title'
+                },
+                {
+                    data: 'date'
+                },
+                {
+                    data: 'content'
+                },
+                {
+                    data: 'author'
+                },
+                {
+                    data: 'action',
+                    searchable: false,
+                    sortable: false
+                },
+            ],
+            columnDefs: [{
+                className: 'text-center',
+                targets: '_all'
+            }]
+        });
+        
+        $('body').on('click', '#articleDelete', function() {
+            var id = $(this).val();
+            Swal.fire({
+                title: 'Yakin Hapus ?',
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Ya Hapus!',
+                confirmButtonColor: '#FF0000',
+                cancelButtonText: 'Tidak',
+                cancelButtonColor: '#3085d6',
+            }).then(function(result) {
+                if (result.value) {
+                    $.ajax({
+                        type: "DELETE",
+                        url: "/admin/artikel/" + id,
+                        success: function(response) {
+                            if (response.success) {
+                                Toast.fire({
+                                    icon: 'success',
+                                    title: response.success
+                                });
+                                table.ajax.reload();
+                            }
+                            if (response.error) {
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Gagal...',
+                                    text: response.error,
+                                })
+                            }
+                        }
+                    });
+                }
+            })
+        });
+</script>
+@endpush
