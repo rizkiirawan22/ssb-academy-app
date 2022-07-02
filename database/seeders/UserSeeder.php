1b8c8794b80f94c9ab6814d125430fe0efe6603d
Rizki Irawan <rizkiirawan22998@gmail.com>
2022-07-02 14:05:42 +0700

    update database seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = [
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin')
+        ];
+        User::create($user)->assignRole('admin');
+    }
+}
