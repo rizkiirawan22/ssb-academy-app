eeb0055269e3409b38ff3f01a7f30a3f5a3bcc88
Rizki Irawan <rizkiirawan22998@gmail.com>
2022-07-02 14:25:32 +0700

    update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SSB Akademi | Login</title>
-    <link rel="icon" href="{{ asset('img/logo.svg') }}">
+    <link rel="icon" href="{{ asset('img/logo.png') }}">
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <link rel="stylesheet" href="{{ asset('template/plugins/fontawesome-free/css/all.min.cs') }}">
