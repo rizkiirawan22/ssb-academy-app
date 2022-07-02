0bb8a7d9160abb855ebeb596824acd165d5e4aa3
Rizki Irawan <rizkiirawan22998@gmail.com>
2022-07-02 19:31:18 +0700

    update admin coach view

--- resources/views/pages/admin/coach/index.blade.php
+++ resources/views/pages/admin/coach/index.blade.php
@@ -9,7 +9,7 @@
         </div>
         <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="/">Home</a></li>
+                <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
                 <li class="breadcrumb-item active">Data Pelatih</li>
             </ol>
         </div>
@@ -23,7 +23,7 @@
 </div>
 <div class="card">
     <div class="card-body table-responsive">
-        <table id="coachTable" class="table table-bordered table-striped">
+        <table style="width: 100%" id="coachTable" class="table table-bordered table-hover table-striped responsive">
             <thead class="text-center">
                 <tr class="bg-primary">
                     <th width="5%">No</th>
