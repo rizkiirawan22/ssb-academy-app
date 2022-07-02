b043d3269021d5dc4244dc25afb63631477a4efc
Rizki Irawan <rizkiirawan22998@gmail.com>
2022-07-02 14:06:05 +0700

    update layouts

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="shorcut icon" href="{{ asset('img/logo.png') }}">
+    <link rel="shorcut icon" href="{{ asset('img/logo.svg') }}">
     <title>PPS App{{ isset($title) ? ' | ' . $title : '' }}</title>
 
     {{-- css --}}
