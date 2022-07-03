8074d3043d72441a1f21fa732901157caa7851d6
Masred <dwifitriyanto123@gmail.com>
2022-07-03 11:20:48 +0700

    changed title

--- resources/views/pages/admin/organization/index.blade.php
+++ resources/views/pages/admin/organization/index.blade.php
@@ -1,43 +1,41 @@
-@extends('layouts.main', ['title' => 'Data SSB','org' => 'active', 'data' => 'active', 'menu'
-=>
-'menu-open'])
+@extends('layouts.main', ['title' => 'Data Organisasi', 'org' => 'active', 'data' => 'active', 'menu' => 'menu-open'])
 @section('content-header')
-<div class="container-fluid">
-    <div class="row mb-2">
-        <div class="col-sm-6">
-            <h1>Data SSB</h1>
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Data Organisasi</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
+                    <li class="breadcrumb-item active">Data Organisasi</li>
+                </ol>
+            </div>
         </div>
-        <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-                <li class="breadcrumb-item"><a href="/dashboard">Home</a></li>
-                <li class="breadcrumb-item active">Data SSB</li>
-            </ol>
-        </div>
-    </div>
-</div><!-- /.container-fluid -->
+    </div><!-- /.container-fluid -->
 @endsection
 @section('content')
-<div class="card">
-    <div class="card-body table-responsive">
-        <table style="width: 100%" id="orgTable" class="table table-bordered table-hover table-striped responsive">
-            <thead class="text-center">
-                <tr class="bg-primary">
-                    <th>Logo</th>
-                    <th>Nama</th>
-                    <th>Visi</th>
-                    <th>Misi</th>
-                    <th>Alamat</th>
-                    <th>Tentang</th>
-                    <th width="10%"><i class="fas fa-cog"></i></th>
-                </tr>
-            </thead>
-        </table>
+    <div class="card">
+        <div class="card-body table-responsive">
+            <table style="width: 100%" id="orgTable" class="table table-bordered table-hover table-striped responsive">
+                <thead class="text-center">
+                    <tr class="bg-primary">
+                        <th>Logo</th>
+                        <th>Nama</th>
+                        <th>Visi</th>
+                        <th>Misi</th>
+                        <th>Alamat</th>
+                        <th>Tentang</th>
+                        <th width="10%"><i class="fas fa-cog"></i></th>
+                    </tr>
+                </thead>
+            </table>
+        </div>
     </div>
-</div>
 @endsection
 @push('js')
-<script type="text/javascript">
-    var Toast = Swal.mixin({
+    <script type="text/javascript">
+        var Toast = Swal.mixin({
             toast: true,
             position: 'top-end',
             showConfirmButton: false,
@@ -46,8 +44,8 @@
 
         @if (session('success'))
             Toast.fire({
-            icon: 'success',
-            title: '{!! session('success') !!}'
+                icon: 'success',
+                title: '{!! session('success') !!}'
             });
         @endif
 
@@ -58,8 +56,7 @@
             ajax: {
                 url: '{{ route('admin.organisasi.index') }}',
             },
-            columns: [
-                {
+            columns: [{
                     data: 'logo'
                 },
                 {
@@ -88,5 +85,5 @@
                 targets: '_all'
             }]
         });
-</script>
+    </script>
 @endpush
